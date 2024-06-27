93cb2c9ad4c873d3165d6e5f4adced3434737cea
moin khan <116509813+MoinKhan2000@users.noreply.github.com>
2024-06-27 09:51:19 +0530

    Started my 5th week from 6/27/2024 In this section we will cover authentication and authorization in our api

---
+++ 5 Week/2 Securing API/1 Understanding Security/index.js
@@ -0,0 +1,15 @@
+// Authentication :
+/*
+    1. Verifying User's Identity.
+    2. Confirms that a user is who they claim to be.
+    3. Examples includes verifying credentials, tokens.
+*/
+
+
+
+// Authorization : 
+/*
+    1. Granting or denying access to specific resources based on user's privileges.
+    2. Controls access to resources based on the user's privileges.
+    3. Examples include checking if the authenticaded user has the permissions to access the resources
+*/
